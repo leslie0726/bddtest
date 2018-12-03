28a94f918aa90a94eddcf86bf61d6928850e6232
leslie <event1511252@gmail.com>
2018-12-03 17:45:59 +0800

    implement first method

---
+++ src/test/java/com/leslie/cucumber/Hello.java
@@ -0,0 +1,11 @@
+package com.leslie.cucumber;
+
+public class Hello {
+
+	private final String greeting;
+	
+	public Hello(String greeting) {
+		this.greeting = greeting;
+	}
+	
+}
--- src/test/java/com/leslie/cucumber/HelloStepdefs.java
+++ src/test/java/com/leslie/cucumber/HelloStepdefs.java
@@ -6,10 +6,13 @@ import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 
 public class HelloStepdefs {
+	Hello hello = null;
+	
 
 	@Given("^I have a greeting application with \"([^\"]*)\"$")
 	public void I_have_a_greeting_application_with(String arg1) throws Throwable{
-		throw new PendingException();
+		hello = new Hello(arg1);
+		//		throw new PendingException();
 	}
 	
 	@When("^I ask it to say hi$")
