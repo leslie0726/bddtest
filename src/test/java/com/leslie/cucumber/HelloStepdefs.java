de5f01d43bee4978a4e488676ef3a3cf729e6015
leslie <event1511252@gmail.com>
2018-12-03 17:33:54 +0800

    implement

---
+++ src/test/java/com/leslie/cucumber/HelloStepdefs.java
@@ -0,0 +1,24 @@
+package com.leslie.cucumber;
+
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class HelloStepdefs {
+
+	@Given("^I have a greeting application with \"([^\"]*)\"$")
+	public void I_have_a_greeting_application_with(String arg1) throws Throwable{
+		throw new PendingException();
+	}
+	
+	@When("^I ask it to say hi$")
+	public void I_ask_it_to_say_hi() throws Throwable{
+		throw new PendingException();
+	}
+	
+	@Then("^I receive \"([^\"]*)\"$")
+	public void I_receive(String arg1) throws Throwable{
+		throw new PendingException();
+	}
+}
