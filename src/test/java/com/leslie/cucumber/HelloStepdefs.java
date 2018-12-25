f58f91280788d4f2de6f109e9bc038fb502769c0
leslie <event1511252@gmail.com>
2018-12-25 12:03:07 +0800

    ref #13 cucumber版本更新

--- src/test/java/com/leslie/cucumber/HelloStepdefs.java
+++ src/test/java/com/leslie/cucumber/HelloStepdefs.java
@@ -2,31 +2,48 @@ package com.leslie.cucumber;
 
 import static org.junit.Assert.assertEquals;
 
-import cucumber.api.PendingException;
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import cucumber.api.java8.En;
 
-public class HelloStepdefs {
+
+
+
+public class HelloStepdefs implements En {
 	Hello hello = null;
 	String hi = null;
 	
+	public HelloStepdefs() {
+		Given("I have a greeting application with {string}", (String string) -> {
+			hello = new Hello(string);
+		});
+		
+		When("I ask it to say hi", () -> {
+			hi = hello.sayHi();
+		});
 
-	@Given("^I have a greeting application with \"([^\"]*)\"$")
-	public void I_have_a_greeting_application_with(String arg1) throws Throwable{
-		hello = new Hello(arg1);
-		//		throw new PendingException();
+		Then("I receive {string}", (String string) -> {
+			assertEquals(string, hi);
+		});
 	}
 	
-	@When("^I ask it to say hi$")
-	public void I_ask_it_to_say_hi() throws Throwable{
-		hi = hello.sayHi();
-//		throw new PendingException();
-	}
 	
-	@Then("^I receive \"([^\"]*)\"$")
-	public void I_receive(String arg1) throws Throwable{
-		assertEquals(arg1, hi);
-//		throw new PendingException();
-	}
+	
+	
+//	
+//	@Given("^I have a greeting application with \"([^\"]*)\"$")
+//	public void I_have_a_greeting_application_with(String arg1) throws Throwable{
+//		hello = new Hello(arg1);
+//		//		throw new PendingException();
+//	}
+//	
+//	@When("^I ask it to say hi$")
+//	public void I_ask_it_to_say_hi() throws Throwable{
+//		hi = hello.sayHi();
+////		throw new PendingException();
+//	}
+//	
+//	@Then("^I receive \"([^\"]*)\"$")
+//	public void I_receive(String arg1) throws Throwable{
+//		assertEquals(arg1, hi);
+////		throw new PendingException();
+//	}
 }
