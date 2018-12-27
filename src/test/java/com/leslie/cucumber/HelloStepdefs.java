b632603a157ac77f83792c9e63612d9680fe17ad
leslie <event1511252@gmail.com>
2018-12-27 10:13:53 +0800

    ref #14 刪掉註解程式碼

--- src/test/java/com/leslie/cucumber/HelloStepdefs.java
+++ src/test/java/com/leslie/cucumber/HelloStepdefs.java
@@ -4,9 +4,6 @@ import static org.junit.Assert.assertEquals;
 
 import cucumber.api.java8.En;
 
-
-
-
 public class HelloStepdefs implements En {
 	Hello hello = null;
 	String hi = null;
@@ -25,25 +22,4 @@ public class HelloStepdefs implements En {
 		});
 	}
 	
-	
-	
-	
-//	
-//	@Given("^I have a greeting application with \"([^\"]*)\"$")
-//	public void I_have_a_greeting_application_with(String arg1) throws Throwable{
-//		hello = new Hello(arg1);
-//		//		throw new PendingException();
-//	}
-//	
-//	@When("^I ask it to say hi$")
-//	public void I_ask_it_to_say_hi() throws Throwable{
-//		hi = hello.sayHi();
-////		throw new PendingException();
-//	}
-//	
-//	@Then("^I receive \"([^\"]*)\"$")
-//	public void I_receive(String arg1) throws Throwable{
-//		assertEquals(arg1, hi);
-////		throw new PendingException();
-//	}
 }
